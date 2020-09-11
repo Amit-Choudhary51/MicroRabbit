8f1de7768d4759a60675707023fe5ef15077755a
amit.choudhary <amit.choudhary@globallogic.com>
2020-09-11 14:59:53 +0530

    Commit Dependency Container

---
+++ MicroRabbit/MicroRabbit.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MicroRabbit.Infra.Ioc
+{
+    public static class DependencyContainer
+    {
+        public static void RegisterDepedency(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
